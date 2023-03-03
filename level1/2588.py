5979a7f3f9526094cb1ffe17a6d5db0616b3042d
HWISU96 <soshyzx123@naver.com>
2023-03-03 15:35:05 +0900

    2588번

--- level1/2588.py
+++ level1/2588.py
@@ -1,13 +1,9 @@
 a = int(input())
 b = input()
 
-
 AB1 = a * int(b[2])
 AB2 = a * int(b[1])
 AB3 = a * int(b[0])
 AB = a * int(b)
 
-print(AB1)
-print(AB2)
-print(AB3)
-print(AB)
+print(AB1, AB2, AB3, AB, sep='\n')
